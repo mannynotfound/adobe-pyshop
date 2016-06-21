d558bb9949b5be1d43bcbca87893279e2eb5faf5
mannynotfound <manny@art404.com>
2016-06-21 19:41:46 -0400

    add batch functionality

--- README.md
+++ README.md
@@ -5,7 +5,32 @@
 * [ImageMagick](http://stackoverflow.com/questions/7053996/how-do-i-install-imagemagick-with-homebrew/27862954#27862954)
 * [cv2 for python](http://www.pyimagesearch.com/2015/06/15/install-opencv-3-0-and-python-2-7-on-osx/)
 
+## usage
+
+Single masker:
+
 ```bash
 python adobe-py.py -b "images/billboard.png" -i "images/subway.jpg" -c "[(135, 140), (462, 30), (488, 256), (97, 328)]" -o "images/lolpy.png"
+```
+
+Batch masker:
+
+Set up a `.json` file like so:
+
+```js
+[
+  {
+    "base": "./images/billboard.png",
+    "inputs": "./images/subway.jpg",
+    "output": "./output/billboard_final.png",
+    "coords": "[(135, 140), (462, 30), (488, 256), (97, 328)]"
+  }
+]
+```
+
+and then just run with `-ba` or `--batch`:
+
+```bash
+python adobe-py.py --batch ./batch.json
 ```
 
--- adobe-py.py
+++ adobe-py.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import argparse
 import os
+import json
 from transform_image import map_perspective
 from show import show_image
 
@@ -10,18 +11,34 @@ ap.add_argument('-b', '--base', help = 'base image to be manipulated')
 ap.add_argument('-i', '--inputs', help = 'input image to apply to base')
 ap.add_argument('-c', '--coords', help = 'cords of where to place input')
 ap.add_argument('-o', '--output', help = 'results output path')
+ap.add_argument('-ba', '--batch', help = 'config file for batch processing')
 args = vars(ap.parse_args())
 
-base = args['base']
-inputs = args['inputs']
-coords = args['coords']
-output = args['output']
+batch = args['batch']
 
-map_perspective(base, inputs, coords, output)
+def mask_image(base, inputs, coords, output):
+    map_perspective(base, inputs, coords, output)
+    os.system('convert ' + output + ' -transparent black ' + output)
+    os.system('convert ' + base + ' ' + output + ' -gravity center -composite ' + output)
 
-os.system('convert ' + output + ' -transparent black ' + output)
-os.system('convert ' + base + ' ' + output + ' -gravity center -composite ' + output)
+if batch:
+    with open(batch) as data:
+        config = json.load(data)
 
-show_image(output)
+        for cfg in config:
+            b = cfg['base']
+            i = cfg['inputs']
+            c = cfg['coords']
+            o = cfg['output']
+            mask_image(b, i, c, o)
+
+    # run through config file
+else:
+    b = args['base']
+    i = args['inputs']
+    c = args['coords']
+    o = args['output']
+    mask_image(b, i, c, o)
+
 
 
